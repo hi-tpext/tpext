7d33f647e4a2bddfb49625db0dd028365da97a09
ichynul <ichynul@163.com>
2022-07-09 13:53:44 +0800

    增强parseTemplate

--- src/think/View.php
+++ src/think/View.php
@@ -4,6 +4,7 @@ namespace tpext\think;
 
 use think\Response;
 use think\Template;
+use think\helper\Str;
 
 class View extends Response
 {
@@ -13,6 +14,11 @@ class View extends Response
 
     protected $isContent = false;
 
+    /**
+     * Undocumented variable
+     *
+     * @var \think\App;
+     */
     protected $app;
 
     /**
@@ -22,18 +28,27 @@ class View extends Response
      */
     protected $engine;
 
+    protected $config = [
+        'auto_rule'     => 1,
+        'view_dir_name' => 'view',
+        'view_path'     => '',
+        'view_suffix'   => 'html',
+        'view_depr'     => DIRECTORY_SEPARATOR,
+        'tpl_cache'     => true,
+    ];
+
     public function __construct($data = '', $vars = [])
     {
         $this->data = $data;
         $this->vars = $vars;
 
-        $config = [
-            'cache_path'     => App::getRuntimePath() . 'temp' . DIRECTORY_SEPARATOR,
-            'view_suffix'   => 'html',
-            'tpl_cache'     => true,
-        ];
-        
-        $this->engine = new Template($config);
+        $this->app = app();
+
+        $this->config['cache_path'] = $this->app->getRuntimePath() . 'temp' . DIRECTORY_SEPARATOR;
+        $this->config['view_path'] = $this->app->getAppPath() . 'view' . DIRECTORY_SEPARATOR;
+
+        $this->engine = new Template($this->config);
+        $this->engine->setCache($this->app->cache);
     }
 
     protected function output($data = '')
@@ -94,6 +109,11 @@ class View extends Response
             if ($this->isContent) {
                 $this->engine->display($template, $vars);
             } else {
+                if ('' == pathinfo($template, PATHINFO_EXTENSION)) {
+                    // 获取模板文件名
+                    $template = $this->parseTemplate($template);
+                }
+
                 $this->engine->fetch($template, $vars);
             }
         } catch (\Exception $e) {
@@ -104,5 +124,73 @@ class View extends Response
         $content = ob_get_clean();
 
         return $content;
+    }
+
+    /**
+     * 自动定位模板文件
+     * @access private
+     * @param  string $template 模板文件规则
+     * @return string
+     */
+    private function parseTemplate(string $template): string
+    {
+        // 分析模板文件规则
+        $request = $this->app['request'];
+
+        // 获取视图根目录
+        if (strpos($template, '@')) {
+            // 跨模块调用
+            list($app, $template) = explode('@', $template);
+        }
+
+        if (isset($app)) {
+            $view     = $this->config['view_dir_name'];
+            $viewPath = $this->app->getBasePath() . $app . DIRECTORY_SEPARATOR . $view . DIRECTORY_SEPARATOR;
+
+            if (is_dir($viewPath)) {
+                $path = $viewPath;
+            } else {
+                $path = $this->app->getRootPath() . $view . DIRECTORY_SEPARATOR . $app . DIRECTORY_SEPARATOR;
+            }
+
+            $this->template->view_path = $path;
+        } else {
+            $path = $this->config['view_path'];
+        }
+
+        $depr = $this->config['view_depr'];
+
+        if (0 !== strpos($template, '/')) {
+            $template   = str_replace(['/', ':'], $depr, $template);
+            $controller = $request->controller();
+
+            if (strpos($controller, '.')) {
+                $pos        = strrpos($controller, '.');
+                $controller = substr($controller, 0, $pos) . '.' . Str::snake(substr($controller, $pos + 1));
+            } else {
+                $controller = Str::snake($controller);
+            }
+
+            if ($controller) {
+                if ('' == $template) {
+                    // 如果模板文件名为空 按照默认模板渲染规则定位
+                    if (2 == $this->config['auto_rule']) {
+                        $template = $request->action(true);
+                    } elseif (3 == $this->config['auto_rule']) {
+                        $template = $request->action();
+                    } else {
+                        $template = Str::snake($request->action());
+                    }
+
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $template;
+                } elseif (false === strpos($template, $depr)) {
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $template;
+                }
+            }
+        } else {
+            $template = str_replace(['/', ':'], $depr, substr($template, 1));
+        }
+
+        return $path . ltrim($template, '/') . '.' . ltrim($this->config['view_suffix'], '.');
     }
 }
