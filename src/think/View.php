3703ea2c5e532a2344e6435fb8d0d6bf8cfd5755
ichynul <ichynul@163.com>
2022-07-09 13:53:53 +0800

    增强parseTemplate

--- src/think/View.php
+++ src/think/View.php
@@ -4,6 +4,7 @@ namespace tpext\think;
 
 use think\Response;
 use think\Template;
+use think\Loader;
 
 class View extends Response
 {
@@ -13,6 +14,11 @@ class View extends Response
 
     protected $isContent = false;
 
+    /**
+     * Undocumented variable
+     *
+     * @var \think\App;
+     */
     protected $app;
 
     /**
@@ -22,13 +28,24 @@ class View extends Response
      */
     protected $engine;
 
+    // 模板引擎参数
+    protected $config = [
+        'auto_rule'   => 1,
+        'view_base'   => '',
+        'view_path'   => '',
+        'view_suffix' => 'html',
+        'view_depr'   => DIRECTORY_SEPARATOR,
+        'tpl_cache'   => true,
+    ];
+
     public function __construct($data = '', $vars = [])
     {
         $this->data = $data;
         $this->vars = $vars;
 
         $this->app = app();
-        $this->engine = new Template($this->app);
+        $this->config['view_path'] = $this->app->getModulePath() . 'view' . DIRECTORY_SEPARATOR;
+        $this->engine = new Template($this->app, $this->config);
     }
 
     protected function output($data = '')
@@ -89,6 +106,10 @@ class View extends Response
             if ($this->isContent) {
                 $this->engine->display($template, $vars);
             } else {
+                if ('' == pathinfo($template, PATHINFO_EXTENSION)) {
+                    // 获取模板文件名
+                    $template = $this->parseTemplate($template);
+                }
                 $this->engine->fetch($template, $vars);
             }
         } catch (\Exception $e) {
@@ -99,5 +120,59 @@ class View extends Response
         $content = ob_get_clean();
 
         return $content;
+    }
+
+    /**
+     * 自动定位模板文件
+     * @access private
+     * @param  string $template 模板文件规则
+     * @return string
+     */
+    private function parseTemplate($template)
+    {
+        // 分析模板文件规则
+        $request = $this->app['request'];
+
+        // 获取视图根目录
+        if (strpos($template, '@')) {
+            // 跨模块调用
+            list($module, $template) = explode('@', $template);
+        }
+
+        if ($this->config['view_base']) {
+            // 基础视图目录
+            $module = isset($module) ? $module : $request->module();
+            $path   = $this->config['view_base'] . ($module ? $module . DIRECTORY_SEPARATOR : '');
+        } else {
+            $path = isset($module) ? $this->app->getAppPath() . $module . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR : $this->config['view_path'];
+        }
+
+        $depr = $this->config['view_depr'];
+
+        if (0 !== strpos($template, '/')) {
+            $template   = str_replace(['/', ':'], $depr, $template);
+            $controller = Loader::parseName($request->controller());
+
+            if ($controller) {
+                if ('' == $template) {
+                    // 如果模板文件名为空 按照默认规则定位
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $this->getActionTemplate($request);
+                } elseif (false === strpos($template, $depr)) {
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $template;
+                }
+            }
+        } else {
+            $template = str_replace(['/', ':'], $depr, substr($template, 1));
+        }
+
+        return $path . ltrim($template, '/') . '.' . ltrim($this->config['view_suffix'], '.');
+    }
+
+    protected function getActionTemplate($request)
+    {
+        $rule = [$request->action(true), Loader::parseName($request->action(true)), $request->action()];
+        $type = $this->config['auto_rule'];
+
+        return isset($rule[$type]) ? $rule[$type] : $rule[0];
     }
 }
