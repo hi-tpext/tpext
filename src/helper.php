c3c64185a4721671b586db8f1172e02d43266024
ichynul <ichynul@163.com>
2024-04-08 09:50:45 +0800

    增加tpInput方法

--- src/helper.php
+++ src/helper.php
@@ -1,13 +1,5 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: yunwuxin <448901948@qq.com>
-// +----------------------------------------------------------------------
+
 use tpext\common\ExtLoader;
 
 ExtLoader::watch('app_init', tpext\common\behavior\AppInit::class, true, '初始化tpext');
@@ -17,3 +9,18 @@ $classMap = [
 ];
 
 ExtLoader::addClassMap($classMap);
+
+if (!function_exists('tpInput')) {
+    /**
+     * 获取输入数据 支持默认值和过滤
+     * (tp框架用法，用于webman等框架兼容)
+     * @param string    $key 获取的变量名
+     * @param mixed     $default 默认值
+     * @param string    $filter 过滤方法
+     * @return mixed
+     */
+    function tpInput($key = '', $default = null, $filter = '')
+    {
+        return input($key, $default, $filter);
+    }
+}
