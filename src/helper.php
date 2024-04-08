a2d0ea862f09747d88e13cf6dee4775057dd1607
ichynul <ichynul@163.com>
2024-04-08 12:11:21 +0800

    Revert "增加tpInput方法"

--- src/helper.php
+++ src/helper.php
@@ -7,18 +7,3 @@ $classMap = [
 ];
 
 ExtLoader::addClassMap($classMap);
-
-if (!function_exists('tpInput')) {
-    /**
-     * 获取输入数据 支持默认值和过滤
-     * (tp框架用法，用于webman等框架兼容)
-     * @param string    $key 获取的变量名
-     * @param mixed     $default 默认值
-     * @param string    $filter 过滤方法
-     * @return mixed
-     */
-    function tpInput($key = '', $default = null, $filter = '')
-    {
-        return input($key, $default, $filter);
-    }
-}
