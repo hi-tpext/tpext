35ce2696fa439ecf091f8f8190b7f3e46471ca2e
ichynul <ichynul@163.com>
2024-12-16 23:31:00 +0800

    trigger默认返回一个结果

--- src/common/ExtLoader.php
+++ src/common/ExtLoader.php
@@ -170,7 +170,7 @@ class ExtLoader
      * @param boolean $once
      * @return mixed
      */
-    public static function trigger($name, $params = null, $once = false)
+    public static function trigger($name, $params = null, $once = true)
     {
         return Hook::listen($name, $params, $once);
     }
