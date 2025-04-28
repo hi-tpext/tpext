f639a59fa61e8dba6320c4e26dd8d6729975e862
ichynul <ichynul@163.com>
2025-04-28 13:09:45 +0800

    update

--- src/think/View.php
+++ src/think/View.php
@@ -143,7 +143,7 @@ class View
         $action = '';
 
         if ($request->route) {
-            $requestPath = strtolower(rtrim($request->route->getPath(),'[.html]'));
+            $requestPath = strtolower(rtrim($request->route->getPath(), '[.html]'));
             $explode = explode('/', trim($requestPath, '/'));
             $module = $explode[0] ?: 'index';
             $controller  = $explode[1] ?? 'index';
