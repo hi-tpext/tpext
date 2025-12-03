5016bc3b4a42700cccaaeb80ecd94c156e7c8fe1
ichynul <ichynul@163.com>
2025-12-03 10:37:48 +0800

    fix cookie

--- src/webman/CrontrollerInit.php
+++ src/webman/CrontrollerInit.php
@@ -32,7 +32,7 @@ class CrontrollerInit implements MiddlewareInterface
 
         if ($exception = $response->exception()) {
             if ($exception instanceof HttpResponseException) {
-                return $exception->getResponse();
+                return $this->saveCookie($exception->getResponse());
             }
 
             if (!($exception instanceof BusinessException)) {
@@ -59,7 +59,7 @@ class CrontrollerInit implements MiddlewareInterface
         return $response;
     }
 
-    protected function saveCookie(Response $response)
+    protected function saveCookie(Response $response): Response
     {
         $cookies = Cookie::getCookie();
 
@@ -77,6 +77,8 @@ class CrontrollerInit implements MiddlewareInterface
                 $option['samesite'] ?? ''
             );
         }
+
+        return $response;
     }
 
     protected function getResponse(Request $request, callable $next): Response
