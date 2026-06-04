b44b151eac30f19c3d140e5f47ff7dbdd49176d7
ichynul <ichynul@163.com>
2026-06-04 10:58:33 +0800

    提供统一的语言支持

--- src/common/Module.php
+++ src/common/Module.php
@@ -2,6 +2,9 @@
 
 namespace tpext\common;
 
+use think\facade\Lang;
+use tpext\think\App;
+
 class Module extends Extension
 {
     public static $current = '';
@@ -18,6 +21,13 @@ class Module extends Extension
      * @var array
      */
     protected $menus = [];
+
+    /**
+     * 数据库表保护，禁止代码生成以及修改表结构
+     *
+     * @var array 
+     */
+    protected static $protectedTables = [];
 
     /**
      * Undocumented function
@@ -37,6 +47,69 @@ class Module extends Extension
     public function getMenus()
     {
         return $this->menus;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return array
+     */
+    public function getProtectedTables()
+    {
+        $class = get_called_class();
+
+        if (empty(self::$protectedTables[$class])) {
+            $sqlFile = $this->getRoot() . 'data' . DIRECTORY_SEPARATOR . 'install.sql';
+            if (is_file($sqlFile)) {
+                $content = file_get_contents($sqlFile);
+                preg_match_all('/CREATE\s+TABLE(?:\s+IF\s+NOT\s+EXISTS)?\s*`(\w+)`/is', $content, $matches);
+                self::$protectedTables[$class] = isset($matches[1]) && count($matches[1]) > 0 ? $matches[1] : ['_empty_'];
+            } else {
+                self::$protectedTables[$class] = ['_empty_'];
+            }
+        }
+
+        return self::$protectedTables[$class];
+    }
+
+    /**
+     * @param string $name
+     * @param string $app
+     * @return void
+     */
+    final public function loadLang($name, $app = 'admin')
+    {
+        if (!$name) {
+            return;
+        }
+        $file = App::getRootPath() . implode(DIRECTORY_SEPARATOR, ['app', $app, 'lang', App::getDefaultLang(), $this->assetsDirName(), $name . '.php']);
+        if (!is_file($file)) {
+            $file = $this->getRoot() . implode(DIRECTORY_SEPARATOR, [$app, 'lang', App::getDefaultLang(), $name . '.php']);
+        }
+        Lang::load($file);
+    }
+
+    /**
+     * @param string $name
+     * @param string $app
+     * @return array
+     */
+    final public function getLang($name, $app = 'admin')
+    {
+        if (!$name) {
+            return [];
+        }
+
+        $file = App::getRootPath() . implode(DIRECTORY_SEPARATOR, ['app', $app, 'lang', App::getDefaultLang(), $this->assetsDirName(), $name . '.php']);
+        if (!is_file($file)) {
+            $file = $this->getRoot() . implode(DIRECTORY_SEPARATOR, [$app, 'lang', App::getDefaultLang(), $name . '.php']);
+        }
+
+        if (is_file($file)) {
+            return include $file;
+        }
+
+        return [];
     }
 
     /**
