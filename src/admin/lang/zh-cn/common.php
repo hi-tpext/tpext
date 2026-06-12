fedbc1b22122c410cc2811589c7917d12dca94ee
ichynul <ichynul@163.com>
2026-06-12 13:41:21 +0800

    dispatch_jump翻译

--- src/admin/lang/zh-cn/common.php
+++ src/admin/lang/zh-cn/common.php
@@ -5,5 +5,10 @@ return [
         . '不要替换文件或上传新文件到此目录及子目录，' . "\n"
         . '否则刷新扩展资源后文件将还原或丢失，' . "\n"
         . '文件建议传到根目录的`public/static`目录下。',
-    'lower_version_error' => '新版本号低于旧版本号，'
+    'lower_version_error' => '新版本号低于旧版本号，',
+    // 跳转模板 (dispatch_jump.tpl)
+    'go_back' => '返回',
+    'redirect_title' => '跳转提示',
+    'page_auto_redirect' => '页面自动跳转：',
+    'redirect_btn' => '跳转',
 ];
